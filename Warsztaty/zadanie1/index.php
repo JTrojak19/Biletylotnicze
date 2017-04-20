1abab1353ee64b80b721a37a41497fb364e85763
Joanna Trojak <joannatrojak19@gmail.com>
2017-04-20 17:33:07 +0200

    Add form to the index

--- Warsztaty/zadanie1/index.php
+++ Warsztaty/zadanie1/index.php
@@ -1,1 +1,12 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h1>Podaj swoje imię: </h1>
+        <form action='' method="post">
+            <input type='text' name="name" placeholder="Twoje imię">
+        </form>
+    </body>
+</html>
 <?php
