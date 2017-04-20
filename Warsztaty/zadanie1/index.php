32c369e293855e86f8990e985c7eb8ac36b59efc
Joanna Trojak <joannatrojak19@gmail.com>
2017-04-20 17:41:19 +0200

    Add submit button

--- Warsztaty/zadanie1/index.php
+++ Warsztaty/zadanie1/index.php
@@ -5,8 +5,16 @@
     <body>
         <h1>Podaj swoje imię: </h1>
         <form action='' method="post">
-            <input type='text' name="name" placeholder="Twoje imię">
+            <input type='text' name="imie" placeholder="Twoje imię">
+            <br>
+            <br>
+            <input type="submit" value="Submit">
         </form>
     </body>
 </html>
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $name = $_POST['imie']; 
+    echo "Cześć". $name; 
+}
