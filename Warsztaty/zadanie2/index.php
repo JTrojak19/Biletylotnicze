5f9155b400c3961a96e69b9b3762d51e6161b87e
Joanna Trojak <joannatrojak19@gmail.com>
2017-04-21 10:06:04 +0200

    Add random

--- Warsztaty/zadanie2/index.php
+++ Warsztaty/zadanie2/index.php
@@ -26,8 +26,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     $number5 = $_POST['number5']; 
     $number6 = $_POST['number6'];
     
+    $random = random_int(1, 49); 
+    
     if ($number1 > 49 || $number2 > 49 || $number3 > 49 || $number4 > 49 || $number5 > 49 || $number6 > 49)
     {
         echo "Liczba z poza zakresu. Wybierz liczbę mniejszą od 49."; 
+    }
+    else 
+    {
+        
     }
 }
