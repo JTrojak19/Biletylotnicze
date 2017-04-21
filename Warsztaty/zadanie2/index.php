0f8c1c078df30e07502c58e0d91bc2cb54b010ee
Joanna Trojak <joannatrojak19@gmail.com>
2017-04-21 09:26:43 +0200

    Add form

--- Warsztaty/zadanie2/index.php
+++ Warsztaty/zadanie2/index.php
@@ -5,6 +5,15 @@
     <body>
         <h1>Symulator Lotto:</h1>
         <p>Wybierz 6 liczb z zakresu 1-49</p>
+        <form>
+            <input type="text" name="number1" placeholder="Liczba pierwsza"> <br><br>
+            <input type="text" name="number2" placeholder="Liczba druga"> <br> <br>
+            <input type="text" name="number3" placeholder="Liczba trzecia"><br><br>
+            <input type="text" name="number4" placeholder="Liczba czwarta"><br><br>
+            <input type="text" name="number5" placeholder="Liczba piąta"><br><br>
+            <input type="text" name="number6" placeholder="Liczba szósta"><br><br>
+            <input type="submit" value="Submit">
+        </form>
     </body>
 </html>
 <?php
