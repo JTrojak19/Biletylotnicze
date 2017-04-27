a6399c8cd17c41d5f9b45a8a7e25d93a4f1c559d
Joanna Trojak <joannatrojak19@gmail.com>
2017-04-27 18:23:28 +0200

    Include airports.php

--- Warsztaty/flights/includes/form.php
+++ Warsztaty/flights/includes/form.php
@@ -4,7 +4,12 @@
     </head>
     <body>
         <form action="pdf.php" method="post">
-            
+            <select>
+                <?php
+                include "airports.php"; 
+                
+                ?>
+            </select>
         </form>
     </body>
 </html>
