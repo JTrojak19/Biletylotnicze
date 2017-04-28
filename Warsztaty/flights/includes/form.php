2601a553ff899ef7cc28756e54bcc30f5c063f7c
Joanna Trojak <joannatrojak19@gmail.com>
2017-04-28 10:55:14 +0200

    Add arrival select

--- Warsztaty/flights/includes/form.php
+++ Warsztaty/flights/includes/form.php
@@ -1,3 +1,6 @@
+<?php
+include "airports.php"; 
+?>
 <html>
     <head>
         
@@ -9,10 +12,22 @@
                     Lotnisko wylotu: 
                 </option>
                 <?php
-                include "airports.php"; 
                 for ($i = 0; $i < count($airports); $i++)   
                 {
                     echo "<option>" . $airports[$i]['name'] . "</option>"; 
+                }
+                ?>
+            </select>
+            <br>
+            <br>
+            <select name="arrival">
+                <option>
+                    Lotnisko przylotu:
+                </option>
+                <?php
+                for ($j=0; $j<count($airports); $j++)
+                {
+                    echo "<option>" . $airports[$j]['name'] . "</option>"; 
                 }
                 ?>
             </select>
